ba52ee69e7cafe4ebc1674602346423d2fc20aa3
Richard Thomas <richard.thomas@savills.com>
2023-03-03 17:35:39 +0000

    tidy up

--- logic.js
+++ logic.js
@@ -1,12 +1,13 @@
-
-import Shape  from "./shape.js";
 import Mapping from "./mapping.js";
 import {saveToLocal, readFromLocal} from "./localStorageAccess.js";
 import {draw, clearCanvas} from "./canvasAction.js";
+import MouseActions from "./mouseActions.js";
 
 export const canvas = document.getElementById("testingCanvas");
 export const debugMessage = document.getElementById("outputDebug");
 export let loadedMap = new Mapping();
+export let mouseActions = new MouseActions();
+
 
 //*Check if canvas is created - read data from local storage and draw it to the canvas
 if(canvas != null || canvas != undefined){
@@ -20,65 +21,28 @@ if(canvas != null || canvas != undefined){
             var rect = canvas.getBoundingClientRect();
             var x = e.clientX - rect.left;
             var y = e.clientY - rect.top;
-            
  
-      let pos = getMousePos(canvas, e);
+            //let pos = getMousePos(canvas, e);
+            let pos = mouseActions.getMousePos(canvas, e);
 
-                 //Check if a item is selected
-                if(selectItem(pos.x, pos.y) == null || selectItem(pos.x, pos.y) == undefined){
-                  console.log(`no item selected`);
-      
-                  //If no item is selected, create a new item
-                  draw(pos.x, pos.y, false, canvas, loadedMap);
-                  debugMessage.innerHTML = `Nothing Selected`;
+            //Check if a item is selected
+            if(mouseActions.selectItem(pos.x, pos.y) == null || mouseActions.selectItem(pos.x, pos.y) == undefined){
+            console.log(`no item selected`);
 
-                  return;
-                }
+            //If no item is selected, create a new item
+            draw(pos.x, pos.y, false, canvas, loadedMap);
+            debugMessage.innerHTML = `Nothing Selected`;
+
+            return;
+            }
 
       }, false);
 }
-
-//*Gets the mouse position on the canvas
-function getMousePos(canvas, evt) {
-      
-      var rect = canvas.getBoundingClientRect();
-     // console.log(`canvas width: ${canvas.width} canvas height: ${canvas.height}`);
-      return {
-          x: (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
-          y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
-      };
-     
-  }
-
 
 //*Used to reverse the centerpoint calculation when adding new shape to canvas
 function reverseCenterpoint(element){
       element.x = element.x + element.width / 2;
       element.y = element.y + element.height / 2;   
-}
-
-//Select an item on the canvas
-function selectItem(x, y){
-      //Get the item at the x and y coordinates
- //debugger;
- let foundElement = null;
-      loadedMap.map.forEach(element => {
-            const dx = x - element.x;
-            const dy = y - element.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-            if(distance <= element.width + 5){
-                  console.log(`item selected`);
-                 
-                  debugMessage.innerHTML = `Item Selected: ${element}`;
-
-                  foundElement = element;
-            }
-            
-            //If the item is selected, deselect it
-            //If the item is not selected, select it
-      });
-      return foundElement;
-      
 }
 
 const clearButton = document.getElementById("clearButton");
